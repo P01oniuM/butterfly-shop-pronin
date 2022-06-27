ca6351db848c46b33b0c072b528a5cc8d8d3e5f6
P01oniuM <totalchlorine17@gmail.com>
2022-06-27 17:22:02 +0300

    rewrite all

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,17 @@
-console.log('script work!');
+$(document).ready(function()
+{
+  $('.burger').click(function(){
+    $('span:nth-child(1)').toggleClass('first');
+    $('span:nth-child(2)').toggleClass('middle');
+    $('span:nth-child(3)').toggleClass('last');
+  });
+
+  $('.burger').click(function(){
+    $('.menu-burger').toggleClass('menu-active');
+  });
+
+
+
+
+
+});
